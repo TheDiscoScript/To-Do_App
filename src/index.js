6a4fc8eced6b8f32c7fb1b71f0a117311252703c
Willdooo <wollmada@gmail.com>
2021-03-07 08:15:52 +0100

    updated functions + small css change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 let database = [
   {
     id: 1,
-    title: "test1",
+    title: "test23",
     description: "peniz1",
     //todo:{}
   },
@@ -27,36 +27,41 @@ const formProjectDescription = document.getElementById(
   "formProjectDescription"
 );
 cancelProjectButton.addEventListener("click", (e) => {
-  formProjectWrap.setAttribute("id", "formProjectWholeNone");
-  formProjectName.value = "";
-  formProjectDescription.value = "";
+  resetForm();
 });
 
 const createProjectButton = document.getElementById("formProjectCreateProject");
 createProjectButton.addEventListener("click", (e) => {
-  //function to push input into database - but how as an object?
-  let object = {
-    title: formProjectName.value,
-    description: formProjectDescription.value,
-  };
-  database.push(object);
+  if (formProjectName.value == "") return;
+  //let currentDate = Date.now(); --> app can do X number of actions in 1 ms, so probably not worth longrun
+  pushIntoDatabase();
   render();
-  formProjectName.value = "";
-  formProjectDescription.value = "";
-  formProjectWrap.setAttribute("id", "formProjectWholeNone");
+  resetForm();
 });
 
 function render() {
   clearContainer(projectContainer);
-  //later database-description.text = create function
-  database.forEach((project) => {
+  create(database);
+}
+function initialize() {
+  render();
+}
+
+//helpfunctions
+function clearContainer(arg) {
+  arg.innerHTML = "";
+}
+function create(arg) {
+  arg.forEach((project) => {
     const projectDiv = document.createElement("div");
     // přidání ID k tomuhle div z database - credit stackoverflow
-    projectDiv.dataset.projectId = project.id;
+    projectDiv.dataset.projectid = project.id;
     const image1 = document.createElement("img");
     image1.src = "images/editPic.jpg";
+    image1.dataset.edit = project.id;
     const image2 = document.createElement("img");
     image2.src = "images/deletePic.jpg";
+    image2.dataset.delete = project.id;
     const title = document.createElement("p");
     title.textContent = project.title;
     const description = document.createElement("p");
@@ -69,7 +74,27 @@ function render() {
     projectDiv.appendChild(description);
   });
 }
-function clearContainer(arg) {
-  arg.innerHTML = "";
+
+function createID() {
+  let increment = database[database.length - 1];
+  let incrementId = increment.id;
+  let newId = incrementId + 1;
+  return newId;
 }
-render();
+
+function pushIntoDatabase() {
+  let object = {
+    id: createID(),
+    title: formProjectName.value,
+    description: formProjectDescription.value,
+  };
+  database.push(object);
+  return database;
+}
+function resetForm() {
+  formProjectWrap.setAttribute("id", "formProjectWholeNone");
+  formProjectName.value = "";
+  formProjectDescription.value = "";
+}
+//for now
+initialize();
